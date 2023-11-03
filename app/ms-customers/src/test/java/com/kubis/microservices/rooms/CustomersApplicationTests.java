83b9a4e7aee420e66798535f4caeb8fc86cf0de6
Filip <filipkubis@student.agh.edu.pl>
2023-11-03 22:06:45 +0100

    Adding some missing files

---
+++ app/ms-customers/src/test/java/com/kubis/microservices/rooms/CustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubis.microservices.rooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
